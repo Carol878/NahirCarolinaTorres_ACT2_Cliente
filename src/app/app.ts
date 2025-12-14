38842bcd80e7958b400ecd75195d66aac54536b9
carol878 <si.carolinatorres@gmail.com>
2025-12-14 13:35:24 +0100

    commit inicial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.html'
 })
-export class App {
-  protected readonly title = signal('gestion-productos');
-}
+export class App {}
